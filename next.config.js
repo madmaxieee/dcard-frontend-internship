10609ff52515d8e160cb880c76952a6aa5b89ae5
zhuang-jia-xu <76544194+zhuang-jia-xu@users.noreply.github.com>
2022-04-05 22:15:26 +0800

    add export prefix

--- next.config.js
+++ next.config.js
@@ -1,17 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // async redirects() {
-  //   return [
-  //     {
-  //       source: "/",
-  //       // destination: `/users/zhuang-jia-xu/repos`,
-  //       destination: `/users/facebook/repos`,
-  //       // destination: `/users/vercel/repos`,
-  //       permanent: false,
-  //     },
-  //   ];
-  // },
+  assetPrefix: "/dcard-frontend-internship",
 };
 
 module.exports = nextConfig;
