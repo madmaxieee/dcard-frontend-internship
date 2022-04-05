3c2bdb4ad4a9f3920ff4ff879cda32b8cf2b721c
zhuang-jia-xu <76544194+zhuang-jia-xu@users.noreply.github.com>
2022-04-05 22:19:23 +0800

    add base path

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: "/dcard-frontend-internship",
   assetPrefix: "/dcard-frontend-internship",
 };
 
