e744a5e9b7006cdf7b401df39f5a5dc062b4300a
zhuang-jia-xu <76544194+zhuang-jia-xu@users.noreply.github.com>
2022-04-05 14:54:45 +0800

    add 404 for wrong username

--- pages/users/[username]/repos/index.tsx
+++ pages/users/[username]/repos/index.tsx
@@ -1,19 +1,33 @@
 import type { NextPage } from "next";
 
 import { useRouter } from "next/router";
-import { Header } from "components";
 
+import { Header } from "components";
 
 const UserReposPage: NextPage = () => {
   const router = useRouter();
-  const { username } = router.query;
+  const { username: queriedUsername } = router.query;
 
   return (
     <>
       <Header />
-      <p>username: {username}</p>
+      <p>username: {queriedUsername}</p>
     </>
   );
+};
+
+export const getStaticProps = async () => {
+  return {
+    props: {},
+  };
+};
+
+export const getStaticPaths = async () => {
+  const config = require("../../../../config/index.ts");
+  return {
+    paths: [{ params: { username: config.username } }],
+    fallback: false,
+  };
 };
 
 export default UserReposPage;
