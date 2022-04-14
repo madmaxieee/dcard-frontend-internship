8b3c813712ee27012867c9ecef36d99563cb29b5
zhuang-jia-xu <76544194+zhuang-jia-xu@users.noreply.github.com>
2022-04-14 17:43:39 +0800

    dockerize production environment

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: "/dcard-frontend-internship",
-  assetPrefix: "/dcard-frontend-internship",
 };
 
 module.exports = nextConfig;
